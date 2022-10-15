526deddd85703c2cb2b6bc5db7cfc527eb066e38
Andreas Abaidoo <abaidooandy@gmail.com>
2022-10-15 09:14:35 +0000

    mongoose part 3

--- models/campsite.js
+++ models/campsite.js
@@ -1,6 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// create a schema for comments about campsites
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
 // create a schema for campsite
 const campsiteSchema = new Schema({
     name: {
@@ -11,7 +31,8 @@ const campsiteSchema = new Schema({
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema] //add comments array as sub document in campsites schema
 }, {
     timestamps: true
 });
